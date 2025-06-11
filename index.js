b5edbe715861f7076b1f8c58a2925cdf52b9d8ea
Sharukh-Ahmed <sharukhahmed0706@gmail.com>
2025-06-11 16:02:47 +0530

    cors update

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(express.json());
 
 //middleware for handlng CORS Policy
 //option 1: Allpw All origins with default of cors(*)
-app.use(cors());
+// app.use(cors());
 //option 2: Allow Custom Origins
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-// }));
+app.use(cors({
+    origin: 'https://book-store-ashen-delta.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 
 app.get('/', (request, response)=> {
     console.log(request);
